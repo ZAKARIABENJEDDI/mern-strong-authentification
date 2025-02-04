eeb862e4e2e820b7b367adaeed94199de8412e37
ZakariaBj <zakariabenjeddi22@gmail.com>
2025-02-04 19:12:42 +0100

    fix navbar style

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,19 +4,19 @@ const Navbar = () => {
   return (
     <nav className="bg-slate-900 p-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">MyApp</div>
+        <div className="text-white text-lg font-bold">Authentification</div>
         
         <div className="space-x-4">
             <>
               <Link
                 to="/login"
-                className="text-white hover:bg-blue-500 px-4 py-2 rounded"
+                className="text-white hover:text-black hover:bg-white  px-4 py-2 rounded"
               >
                 Login
               </Link>
               <Link
                 to="/register"
-                className="text-white hover:bg-blue-500 px-4 py-2 rounded"
+                className="text-white hover:text-black hover:bg-white px-4 py-2 rounded"
               >
                 Register
               </Link>
