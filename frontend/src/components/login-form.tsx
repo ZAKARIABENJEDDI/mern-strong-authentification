f7bb50ee36e300472cea318585b6b77980f638f8
ZakariaBj <zakariabenjeddi22@gmail.com>
2025-02-05 14:19:35 +0100

    refactor ForgetPassword component; streamline form handling and improve user experience with loading state and input management

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -97,9 +97,6 @@ export function LoginForm() {
               <Button type="submit" className="w-full" disabled={isLoading}>
                 Login
               </Button>
-              <Button variant="outline" className="w-full">
-                Login with Google
-              </Button>
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
--- frontend/src/pages/ForgetPassword.tsx
+++ frontend/src/pages/ForgetPassword.tsx
@@ -5,7 +5,7 @@ import { Label } from "@/components/ui/label"
 import { useState } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
-import { Modal, TextInput } from "flowbite-react";
+import { Modal} from "flowbite-react";
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function Forget() {
@@ -15,14 +15,14 @@ export default function Forget() {
 
   const navigate = useNavigate();
 
-  const handelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target
-  }
-
   const handelSubmit = (e: React.FormEvent<HTMLFormElement>) => { 
     e.preventDefault();
-    setisLoading(true);
+    console.log(email);
     toast.success("ww")
+    setisLoading(true);
+    setTimeout(() => {
+      setisLoading(false);
+    }, 5000);
     axios.post("",code)
     .then((res)=>{
       console.log(res);
@@ -45,47 +45,66 @@ export default function Forget() {
       <Modal show={openModal} size="md" onClose={onCloseModal} popup>
         <Modal.Header />
         <Modal.Body>
-          <div className="space-y-6">
-            <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
-              Remember your password&nbsp;
-              <Link
-                to="/login"
-                className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-              >
-                Login
-              </Link>
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="email" />
+          <form onSubmit={handelSubmit}>
+            <div className="space-y-6">
+              <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
+                Remember your password&nbsp;
+                <Link
+                  to="/login"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Login
+                </Link>
               </div>
-              <Input
-                  name="email"
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="password" value="Your password" />
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="email" />
+                </div>
+                <Input
+                    onChange={(e) => {
+                      setemail(e.target.value)
+                    }}
+                    name="email"
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
               </div>
-              <TextInput id="password" type="password" required />
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="code">Enter Code</Label>
+                </div>
+                <Input
+                    onChange={(e) => {
+                      setCode(e.target.value)
+                    }}
+                    name="code"
+                    id="code"
+                    type="text"
+                    readOnly={!code}
+                    disabled={!code}
+                  />
+              </div>
+              <div className="w-full">
+                <Button 
+                  type="submit"
+                  disabled={isLoading}
+                >
+                  {isLoading ? "..." : "Log in to your account"}
+                </Button>
+              </div>
+              <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
+                Not registered?&nbsp;
+                <Link
+                  to="/register"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Create account
+                </Link>
+              </div>
             </div>
-            <div className="w-full">
-              <Button>Log in to your account</Button>
-            </div>
-            <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
-              Not registered?&nbsp;
-              <Link
-                to="/register"
-                className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-              >
-                Create account
-              </Link>
-            </div>
-          </div>
+          </form>
         </Modal.Body>
       </Modal>
       <ToastContainer />
