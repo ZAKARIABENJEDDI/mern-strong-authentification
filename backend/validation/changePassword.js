26d80f0337743980044b8e6233ee6e04b8785e28
ZakariaBj <zakariabenjeddi22@gmail.com>
2025-02-06 23:14:39 +0100

    add validation for ChangePassword; enhance password checks and error messages

--- backend/validation/changePassword.js
+++ backend/validation/changePassword.js
@@ -3,8 +3,22 @@ const IsEmpty = require("is-empty")
 
 module.exports = ChangePasswordValidation = (data) => {
   const errors = {}
-  data.password = !isEmpty(data.password) ? data.password : ""
+  data.password = !IsEmpty(data.password) ? data.password : ""
+  data.password2 = !IsEmpty(data.password2) ? data.password2 : ""
   if (!Validator.isLength(data.password, {min:6 ,max:30})) {
-    errors.password = "password schuld be more than 6 letters"
+    errors.password = "Password must be at least 6 charachters"
+  }
+
+  if(Validator.isEmpty(data.password)){
+    errors.password = "Enter Password"
+  }
+
+  if (!Validator.equals(data.password,data.password2)) {
+    errors.password2 = "Les Deux Password Doivent Etre Equax"
+  }
+
+  return {
+    errors,
+    isValid: IsEmpty(errors)
   }
 }
